df0a481a872a1233cf0ccb8a98e73e5dcc1ac27a
Chris Clifford and Michael Andrade <chris@>
2016-11-14 14:44:18 -0800

    add code to return degrees for every hour up to 6:00

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,7 +1,15 @@
 class String
   define_method(:clock_angle) do
-    if(self == "12:00")
-      "0°"
+    if (self == "12:00")
+      0
+    else
+      hands = self.split(":")
+      hour_hand = hands.at(0).to_i()
+      minute_hand = hands.at(1).to_i()
+      if (hour_hand <= 6)
+        hour_hand * 30
+      end
+
     end
   end
 end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -3,6 +3,14 @@ require('clock_angle')
 
 describe('String#clock_angle') do
   it('returns 0° if input is the string "12:00"') do
-    expect("12:00".clock_angle).to(eq("0°"))
+    expect("12:00".clock_angle).to(eq(0))
+  end
+
+  it('returns the degree distance for every hour up to 6 as a Fixnum') do
+    expect("6:00".clock_angle).to(eq(180))
+  end
+
+  it('returns the degree distance for every hour between 6 and 12 as a Fixnum') do
+    expect("9:00".clock_angle).to(eq(90))
   end
 end
