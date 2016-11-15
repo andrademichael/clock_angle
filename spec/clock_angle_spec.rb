499a41ca436dcccc78f7c4cf71f40330222d1bde
Chris Clifford and Michael Andrade <chris@>
2016-11-14 16:03:56 -0800

    change the wording in a spec

--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -7,15 +7,15 @@ describe('String#clock_angle') do
     expect("12:00".clock_angle).to(eq(0))
   end
 
-  it('returns the degree distance for every hour up to 6 as a Fixnum') do
+  it('returns the degree distance for every hour up to 6 as a Float') do
     expect("6:00".clock_angle).to(eq(180))
   end
 
-  it('returns the degree distance for every hour between 6 and 12 as a Fixnum') do
+  it('returns the degree distance for every hour between 6 and 12 as a Float') do
     expect("9:00".clock_angle).to(eq(90))
   end
 
-  it('returns the degree distance for every minute after 0 relative to the hour handas a Fixnum') do
+  it('returns the distance between the hour and minute hand in degrees') do
     expect("2:30".clock_angle).to(eq(105))
   end
 end
